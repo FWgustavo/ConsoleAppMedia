55c02e4e751f4f9f7d48d0dad5bdada52c8e65dd
Usuario <Usuario@CPD02-M01>
2024-03-12 11:33:51 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppMedia/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppMedia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva a média do aluno: ");
+            double media = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escreva a taxa de presença (%) do aluno: ");
+            int porcentagem = int.Parse(Console.ReadLine());
+
+            if (media >= 7 && porcentagem >= 75)
+            {
+                Console.WriteLine("Aluno aprovado");
+            }
+            else if (media < 7 && media > 4 || porcentagem <= 75)
+            {
+                Console.WriteLine("Aluno de Recuperação");
+                Console.ReadLine();
+
+                Console.WriteLine("Escreva a nota da Recuperação");
+                double rec = double.Parse(Console.ReadLine());
+
+                if (rec >= 7)
+                {
+                    Console.WriteLine("Nota Recuperada, o Aluno Passou");
+                }
+                else
+                {
+                    Console.WriteLine("Aluno Reprovado");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Aluno Reprovado!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
